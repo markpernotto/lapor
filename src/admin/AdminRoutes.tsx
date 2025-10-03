6b5859c47eb914fae52727c942777afbda3119eb
Mark Pernotto <mark@pernotto.com>
2025-10-02 22:19:51 -0700

    fix routing between admin and survey view

--- src/admin/AdminRoutes.tsx
+++ src/admin/AdminRoutes.tsx
@@ -1,18 +1,46 @@
-import { Routes, Route } from "react-router-dom";
+import {
+  Routes,
+  Route,
+  Link,
+  Outlet,
+  BrowserRouter,
+} from "react-router-dom";
 import QuestionsPage from "./QuestionsPage";
 import SurveysPage from "./SurveysPage";
+
+function Shell() {
+  return (
+    <div>
+      <header className="app-header">
+        <h1>Lapor Admin</h1>
+        <nav>
+          <Link to="/admin/questions">
+            Questions
+          </Link>
+          {" | "}
+          <Link to="/admin/surveys">Surveys</Link>
+        </nav>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
+}
 
 export default function AdminRoutes() {
   return (
     <Routes>
-      <Route
-        path="/admin/questions"
-        element={<QuestionsPage />}
-      />
-      <Route
-        path="/admin/surveys"
-        element={<SurveysPage />}
-      />
+      <Route path="/admin" element={<Shell />}>
+        <Route
+          path="questions"
+          element={<QuestionsPage />}
+        />
+        <Route
+          path="surveys"
+          element={<SurveysPage />}
+        />
+      </Route>
     </Routes>
   );
 }
