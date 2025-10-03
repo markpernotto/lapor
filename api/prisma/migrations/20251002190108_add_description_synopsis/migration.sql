01b54270ebb699a3cb216c9264cc0aa71c0c324a
Mark Pernotto <mark@pernotto.com>
2025-10-02 23:48:35 -0700

    fix migrations? maybe?

--- api/prisma/migrations/20251002190108_add_description_synopsis/migration.sql
+++ api/prisma/migrations/20251002190108_add_description_synopsis/migration.sql
@@ -1,3 +1,6 @@
 -- Add description and synopsis to Survey
 ALTER TABLE "Survey" ADD COLUMN "description" TEXT;
 ALTER TABLE "Survey" ADD COLUMN "synopsis" TEXT;
+-- Add description and synopsis to Survey
+ALTER TABLE "Survey" ADD COLUMN "description" TEXT;
+ALTER TABLE "Survey" ADD COLUMN "synopsis" TEXT;
